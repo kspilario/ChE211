c755856e26dfe112e86fb1edd84eba5364323707
Karl Ezra Pilario <kspilario@up.edu.ph>
2025-09-17 12:24:49 +0800

    Add files via upload

---
+++ Week 6 - Opti with DAEs/illustrate_poly_interp2.m
@@ -0,0 +1,37 @@
+
+tm = [0; roots([6 -6 1]); 1]; % Roots of shifted Legendre polynomial
+disp('Collocation pts.')
+disp(tm); rng(0);
+
+t = linspace(0, 1, 200); n = 10;
+xi = randi(6,[n 4])-3;
+xi = [xi; xi(1,:)];
+
+% Basis functions
+phi1 = @(t) (t-tm(2))./(tm(1)-tm(2)).*(t-tm(3))./(tm(1)-tm(3)).*(t-tm(4))./(tm(1)-tm(4));
+phi2 = @(t) (t-tm(1))./(tm(2)-tm(1)).*(t-tm(3))./(tm(2)-tm(3)).*(t-tm(4))./(tm(2)-tm(4));
+phi3 = @(t) (t-tm(1))./(tm(3)-tm(1)).*(t-tm(2))./(tm(3)-tm(2)).*(t-tm(4))./(tm(3)-tm(4));
+phi4 = @(t) (t-tm(1))./(tm(4)-tm(1)).*(t-tm(2))./(tm(4)-tm(2)).*(t-tm(3))./(tm(4)-tm(3));
+
+xi_fine = []; phi = {phi1, phi2, phi3, phi4};
+for j = 1:4
+    xi_fine(:,j) = interp1(0:n, xi(:,j), ...
+        linspace(0,n,300), 'cubic'); %#ok
+end
+filename = 'poly_interp2.gif'; f = figure;
+for j = 1:height(xi_fine)
+
+    % Interpolation of x(t)
+
+    for k = 1:4
+        xt = xi_fine(j,k)*phi{k}(t);
+        subplot(4,1,k);
+        plot(t, xt, 'k', 'LineWidth', 1.2); 
+        hold on; set(gcf, 'Color','w');
+        scatter(tm(k), xi_fine(j,k), 'm', 'filled'); 
+        hold off; grid on; ylim([-5 5]); 
+    end
+    
+    pause(0.2);
+    exportgraphics(f, filename, 'Append', true);
+end
